5795100965f9b18b108a9e22ffff032ee790a34a
Tsunghao Huang <huang.tsunghao@gmail.com>
2020-07-19 18:47:50 +0200

    change to asynchronous

--- choropleth-map/script.js
+++ choropleth-map/script.js
@@ -4,81 +4,152 @@ const padding = { top: 10, right: 80, bottom: 50, left: 80 };
 
 const promise1 = d3.json('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json');
 const promise2 = d3.json('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json');
+const section = d3.select('body').append('section');
+const heading = section.append('heading');
+heading.append('h1')
+  .attr('id', 'title')
+  .attr('text-anchor', 'middle')
+  .attr('x', w / 2)
+  .attr('y', 10)
+  .text('United States Educational Attainment');
+heading.append('h3')
+  .attr('id', 'description')
+  .attr('text-anchor', 'middle')
+  .attr('x', w / 2)
+  .text('Percentage of adults age 25 and older with a bachelor\'s degree or higher (2010-2014)');
+
+const svg = d3.select('body').append('svg')
+  .attr('width', w)
+  .attr('height', h);
+const path = d3.geoPath();
+const tooltip = d3.select('body').append('div')
+  .attr('id', 'tooltip')
+  .style('opacity', 0);
+
+//legend
+var colors = [];
+var legendTicks = [];
+for (var i = 0; i < 9; i++) {
+  const scale = 10 / 8;
+  var threshold = (i * 0.1).toFixed(1);
+  colors = colors.concat(d3.interpolateOrRd(threshold * scale));
+  legendTicks = legendTicks.concat(threshold);
+}
+
+const colorFunc = (t) => {
+  const colorInd = legendTicks.findIndex((d) => d >= t) - 1;
+  return colors[colorInd];
+}
+const legendRectW = 36;
+const legendRectH = 8;
+const legend = svg.append('g')
+  .attr('transform', `translate(${w - padding.right - legendRectW * colors.length}, ${2 * legendRectH})`)
+  .attr('id', 'legend')
+
+legend.selectAll('rect')
+  .data(colors)
+  .enter()
+  .append('rect')
+  .attr('fill', (d) => d)
+  .attr('width', legendRectW)
+  .attr('height', legendRectH)
+  .attr('x', (d, i) => i * legendRectW)
+  .attr('stroke', 'gray');
+
+legend.selectAll('text')
+  .data(legendTicks)
+  .enter()
+  .append('text')
+  .text((t, i) => {
+    switch (i) {
+      case 0:
+        return '';
+        break;
+      case (legendTicks.length - 1):
+        return t * 100 + '%';
+        break;
+      default:
+        return t * 100;
+    }
+  })
+  .attr('x', (d, i) => i * legendRectW - 6)
+  .attr('y', -legendRectH / 3)
+  .style("font-size", "12px")
 
 Promise.all([promise1, promise2])
   .then(([edu, us]) => {
 
-    const section = d3.select('body').append('section');
-    const heading = section.append('heading');
-    heading.append('h1')
-      .attr('id', 'title')
-      .attr('text-anchor', 'middle')
-      .attr('x', w / 2)
-      .attr('y', 10)
-      .text('United States Educational Attainment');
-    heading.append('h3')
-      .attr('id', 'description')
-      .attr('text-anchor', 'middle')
-      .attr('x', w / 2)
-      .text('Percentage of adults age 25 and older with a bachelor\'s degree or higher (2010-2014)');
+    // const section = d3.select('body').append('section');
+    // const heading = section.append('heading');
+    // heading.append('h1')
+    //   .attr('id', 'title')
+    //   .attr('text-anchor', 'middle')
+    //   .attr('x', w / 2)
+    //   .attr('y', 10)
+    //   .text('United States Educational Attainment');
+    // heading.append('h3')
+    //   .attr('id', 'description')
+    //   .attr('text-anchor', 'middle')
+    //   .attr('x', w / 2)
+    //   .text('Percentage of adults age 25 and older with a bachelor\'s degree or higher (2010-2014)');
 
-    const svg = d3.select('body').append('svg')
-      .attr('width', w)
-      .attr('height', h);
-    const path = d3.geoPath();
-    const tooltip = d3.select('body').append('div')
-      .attr('id', 'tooltip')
-      .style('opacity', 0);
+    // const svg = d3.select('body').append('svg')
+    //   .attr('width', w)
+    //   .attr('height', h);
+    // const path = d3.geoPath();
+    // const tooltip = d3.select('body').append('div')
+    //   .attr('id', 'tooltip')
+    //   .style('opacity', 0);
 
-    //legend
-    var colors = [];
-    var legendTicks = [];
-    for (var i = 0; i < 9; i++) {
-      const scale = 10 / 8;
-      var threshold = (i * 0.1).toFixed(1);
-      colors = colors.concat(d3.interpolateOrRd(threshold * scale));
-      legendTicks = legendTicks.concat(threshold);
-    }
+    // //legend
+    // var colors = [];
+    // var legendTicks = [];
+    // for (var i = 0; i < 9; i++) {
+    //   const scale = 10 / 8;
+    //   var threshold = (i * 0.1).toFixed(1);
+    //   colors = colors.concat(d3.interpolateOrRd(threshold * scale));
+    //   legendTicks = legendTicks.concat(threshold);
+    // }
 
-    const colorFunc = (t) => {
-      const colorInd = legendTicks.findIndex((d) => d >= t) - 1;
-      return colors[colorInd];
-    }
-    const legendRectW = 36;
-    const legendRectH = 8;
-    const legend = svg.append('g')
-      .attr('transform', `translate(${w - padding.right - legendRectW * colors.length}, ${2 * legendRectH})`)
-      .attr('id', 'legend')
+    // const colorFunc = (t) => {
+    //   const colorInd = legendTicks.findIndex((d) => d >= t) - 1;
+    //   return colors[colorInd];
+    // }
+    // const legendRectW = 36;
+    // const legendRectH = 8;
+    // const legend = svg.append('g')
+    //   .attr('transform', `translate(${w - padding.right - legendRectW * colors.length}, ${2 * legendRectH})`)
+    //   .attr('id', 'legend')
 
-    legend.selectAll('rect')
-      .data(colors)
-      .enter()
-      .append('rect')
-      .attr('fill', (d) => d)
-      .attr('width', legendRectW)
-      .attr('height', legendRectH)
-      .attr('x', (d, i) => i * legendRectW)
-      .attr('stroke', 'gray');
+    // legend.selectAll('rect')
+    //   .data(colors)
+    //   .enter()
+    //   .append('rect')
+    //   .attr('fill', (d) => d)
+    //   .attr('width', legendRectW)
+    //   .attr('height', legendRectH)
+    //   .attr('x', (d, i) => i * legendRectW)
+    //   .attr('stroke', 'gray');
 
-    legend.selectAll('text')
-      .data(legendTicks)
-      .enter()
-      .append('text')
-      .text((t, i) => {
-        switch (i) {
-          case 0:
-            return '';
-            break;
-          case (legendTicks.length - 1):
-            return t * 100 + '%';
-            break;
-          default:
-            return t * 100;
-        }
-      })
-      .attr('x', (d, i) => i * legendRectW - 6)
-      .attr('y', -legendRectH / 3)
-      .style("font-size", "12px")
+    // legend.selectAll('text')
+    //   .data(legendTicks)
+    //   .enter()
+    //   .append('text')
+    //   .text((t, i) => {
+    //     switch (i) {
+    //       case 0:
+    //         return '';
+    //         break;
+    //       case (legendTicks.length - 1):
+    //         return t * 100 + '%';
+    //         break;
+    //       default:
+    //         return t * 100;
+    //     }
+    //   })
+    //   .attr('x', (d, i) => i * legendRectW - 6)
+    //   .attr('y', -legendRectH / 3)
+    //   .style("font-size", "12px")
 
     svg.append("g")
       .attr("class", "counties")
